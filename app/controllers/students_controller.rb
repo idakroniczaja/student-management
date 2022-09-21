ada901da732d7b6b9105a3a1550aac8c5a8a5a32
idakroniczaja <so.kida@hotmail.com>
2022-09-20 21:58:38 -0400

    update and delete the student and partialst

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,43 +1,50 @@
 class StudentsController < ApplicationController 
+
+#callback
     before_action :set_student, only: %i[show edit update destroy]
 
-
-
+#showing the resource
     def index
         @students = Student.all
     end
 
+#creating new resource
     def new
         @student = Student.new
     end
 
+    #saving new resource, captured from the new 
     def create
         @student = Student.new(student_params)
        if @student.save
-        redirect_to students_path
+        redirect_to students_path, notice: 'Student has been created successfully.'
        else
         render :new
        end
     end
 
+#fetching single data
     def show 
     end
 
+#fetching existing data from db and displaying info in the form to edit
     def edit 
     end
 
+#saving new record in db
     def update 
         if @student.update(student_params)
             # redirect_to students_path
-            redirect_to student_path(@student)
+            redirect_to student_path(@student), notice: 'Student has been updated successfully.'
         else
             render :edit
         end
     end
 
+#deleting and it requires redirection
     def destroy 
         @student.destroy
-        redirect_to students_path
+        redirect_to students_path, notice: 'Student has been deleted successfully.'
     end
     
     private 
