fcf56e427820098d3b50f6810173a73cd7510670
idakroniczaja <so.kida@hotmail.com>
2022-09-29 17:57:01 -0400

    student callback

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,9 +6,9 @@ class Student < ApplicationRecord
 
     validates :first_name, :last_name, :email, :date_of_birth, :permanent_contact_number, :local_address, :permanent_address, :alternate_contact_number, presence: true
 
-    # before_create :display_greenings
+    before_create :display_greenings
 
-    # def display_greenings
-    #     puts "Hello, You executed before action callback"
-    # end
+    def display_greenings
+        puts "Hello, You executed before action callback"
+    end
 end
