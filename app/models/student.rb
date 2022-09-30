985c1de50d0d135d4c384d1e4d54ca4e795c23c1
idakroniczaja <so.kida@hotmail.com>
2022-09-30 15:32:13 -0400

    student callbacks 2

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,9 +6,20 @@ class Student < ApplicationRecord
 
     validates :first_name, :last_name, :email, :date_of_birth, :permanent_contact_number, :local_address, :permanent_address, :alternate_contact_number, presence: true
 
-    before_create :display_greenings
+    # before_create :display_greenings
+    after_save :display_student_age
 
-    def display_greenings
-        puts "Hello, You executed before action callback"
+    # def display_greenings
+    #     puts "Hello, You executed before action callback"
+    # end
+
+    def display_student_age
+        if self.date_of_birth.present?
+            age = Date.today.year - self.date_of_birth.year
+            puts "************ Age of the student is #{age}."
+       else
+        puts "******** Age can not be calculated without date_of_birth"
+       end
     end
+
 end
