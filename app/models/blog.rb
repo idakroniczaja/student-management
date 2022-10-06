cf05e385c02887eef6d297dc575089f21a959c42
idakroniczaja <so.kida@hotmail.com>
2022-10-06 09:58:07 -0400

    blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -4,4 +4,10 @@ class Blog < ApplicationRecord
     validates :title, :content, presence: true
     validates :title, uniqueness: true
     validates :content, length: {minimum:10, maximum:1000}
+
+
+    # def show_student
+    #     return @student = Student.find(blog.student_id[:id])
+    # end
+    # @project = Project.find(params[:id])
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :projects
   resources :courses
+  resources :blogs
   #left side is controller name / right side is action name
 
   root 'welcome#index'
